c0fa05b1945908409204225473fd02e9473dd22a
Anatoliy <anatoliypuss5@gmail.com>
2021-02-01 23:40:44 +0300

    python test

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import path from 'path';
+import {spawn} from 'child_process';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    console.log(1);
-    res.sendFile('index');
+    // res.sendFile('index');
+    const python = spawn('python', ['test.py']);
+    python.stdout.on('data', (data) => {
+        res.end(data.toString())
+    })
 })
 
 app.listen(PORT, () => {
