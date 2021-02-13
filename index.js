53c62c6cdeb592422533a17748a7b7b22c6df449
Anatoliy <anatoliypuss5@gmail.com>
2021-02-13 10:32:55 +0300

    fixed keys to db

--- index.js
+++ index.js
@@ -1,52 +1,61 @@
-import express from 'express';
-import getEdaRuRecepies from './parsers/edaParser.js';
-import chalk from 'chalk';
-import checkEdaRAmount from './parsers/checkEdaRAmount.js';
-import mysql from 'mysql';
+import express from "express";
+import getEdaRuRecepies from "./parsers/edaParser.js";
+import chalk from "chalk";
+import checkEdaRAmount from "./parsers/checkEdaRAmount.js";
+import mysql from "mysql";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-const tableName = 'recipes';
+const tableName = "recipes";
 
-app.set('views', 'public');
-app.set('view engine', 'ejs');
+app.set("views", "public");
+app.set("view engine", "ejs");
 
-app.get('/api/parse', async (req, res) => {
-    res.send('Parsing started');
-    const data = await getEdaRuRecepies();
-    const connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
-    connection.connect((e) => {
-        if (e) throw e
-    });
-    connection.query(`DELETE FROM ${tableName}`);
-    for (let el of data) {
-        connection.query(`INSERT INTO ${tableName} (title, url, cook_time, ingredients_amount, steps_arr_json, ingredients_arr_json, images_arr_json, categories_arr_json)
-                            VALUES ('${el.title}', '${el.url}', '${el.time}', '${el.ingredientsAmount}', '${JSON.stringify(el.steps)}', '${JSON.stringify(el.ingredients)}', '${JSON.stringify(el.images)}', '${JSON.stringify(el.categories)}')`, (e) => {
-            if (e) throw e;
-        });
-    }
-});
-
-app.get('/api/get', (req, res) => {
-    const connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
-    connection.connect((e) => {
-        if (e) throw e
-    });
-    connection.query('SELECT * FROM recipes', (e, data) => {
+app.get("/api/parse", async (req, res) => {
+  res.send("Parsing started");
+  const data = await getEdaRuRecepies();
+  const connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
+  connection.connect((e) => {
+    if (e) throw e;
+  });
+  connection.query(`DELETE FROM ${tableName}`);
+  for (let el of data) {
+    connection.query(
+      `INSERT INTO ${tableName} (title, url, cook_time, ingredients_amount, steps_arr_json, ingredients_arr_json, images_arr_json, categories_arr_json)
+                            VALUES ('${el.title}', '${el.url}', '${
+        el.time
+      }', '${el.ingredientsAmount}', '${JSON.stringify(
+        el.steps
+      )}', '${JSON.stringify(el.ings)}', '${JSON.stringify(
+        el.imgs
+      )}', '${JSON.stringify(el.ctgrs)}')`,
+      (e) => {
         if (e) throw e;
-        res.send(data);
-    });
+      }
+    );
+  }
 });
 
-app.get('/api/check', async (req, res) => {
-    const data = await checkEdaRAmount();
+app.get("/api/get", (req, res) => {
+  const connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
+  connection.connect((e) => {
+    if (e) throw e;
+  });
+  connection.query("SELECT * FROM recipes", (e, data) => {
+    if (e) throw e;
     res.send(data);
+  });
 });
 
-app.get('/test', (req, res) => {
-    res.send(JSON.stringify(req.query));
+app.get("/api/check", async (req, res) => {
+  const data = await checkEdaRAmount();
+  res.send(data);
+});
+
+app.get("/test", (req, res) => {
+  res.send(JSON.stringify(req.query));
 });
 
 app.listen(PORT, () => {
-    console.log(chalk.yellow('Server has been started...'));
+  console.log(chalk.yellow("Server has been started..."));
 });
--- parsers/edaParser.js
+++ parsers/edaParser.js
@@ -1,74 +1,76 @@
-import axios from 'axios';
-import { spawn } from 'child_process';
-import fs from 'fs';
-import path from 'path';
-import chalk from 'chalk';
+import axios from "axios";
+import { spawn } from "child_process";
+import fs from "fs";
+import path from "path";
+import chalk from "chalk";
 
-const url = 'https://eda.ru/recepty';
+const url = "https://eda.ru/recepty";
 const pagesAmount = 2; // кол-во страниц для парсинга
-const tempFile = 'parsers/temp.txt';
+const tempFile = "parsers/temp.txt";
 const __dirname = path.resolve();
-const parseProductsFileName = 'parsers/parseEdaProducts.py';
-const parseProductCardFileName = 'parsers/parseEdaProductCard.py';
+const parseProductsFileName = "parsers/parseEdaProducts.py";
+const parseProductCardFileName = "parsers/parseEdaProductCard.py";
 
 export default async function getEdaRuRecepies() {
-    let collection = [];
+  let collection = [];
 
-    // проходит по страницам с продуктами
-    for (let i = 1; i <= pagesAmount; i++) {
-        const response = await axios.get(url + '?page=' + i);
-        const data = response.data;
-        const result = await processAllProducts(data);
-        collection = collection.concat(JSON.parse(result))
-    }
+  // проходит по страницам с продуктами
+  for (let i = 1; i <= pagesAmount; i++) {
+    const response = await axios.get(url + "?page=" + i);
+    const data = response.data;
+    const result = await processAllProducts(data);
+    collection = collection.concat(JSON.parse(result));
+  }
 
-    console.log(chalk.green('Succesfully parsed all products, parsing ingredients...'));
+  console.log(
+    chalk.green("Succesfully parsed all products, parsing ingredients...")
+  );
 
-    // проходит по всем полученным продуктам, достает ингредиенты и шаги по приготовлению
-    for (let i = 0; i < collection.length; i++) {
-        const el = {
-            ...collection[i]
-        };
-        const response = await axios.get(el.url);
-        const data = response.data;
-        const result = await processProductCard(data);
-        collection[i] = {
-            ...el, 
-            ...JSON.parse(result)
-        }
-    }
+  // проходит по всем полученным продуктам, достает ингредиенты и шаги по приготовлению
+  for (let i = 0; i < collection.length; i++) {
+    const el = {
+      ...collection[i],
+    };
+    const response = await axios.get(el.url);
+    const data = response.data;
+    const result = await processProductCard(data);
+    collection[i] = {
+      ...el,
+      ...JSON.parse(result),
+    };
+  }
 
-    fs.unlink(path.join(__dirname, tempFile), (e) => {
-        if (e) throw new Error;
-    });
+  fs.unlink(path.join(__dirname, tempFile), (e) => {
+    if (e) throw new Error();
+  });
 
-    console.log(chalk.green('Parsing done!'));
+  console.log(chalk.green("Parsing done!"));
 
-    return collection
+  return collection;
 }
 
 // создает процесс с питоном, парсит страницу с продуктами
 async function processAllProducts(data) {
-    fs.writeFile(path.join(__dirname, tempFile), data, (e) => {
-        if (e) throw e;
+  fs.writeFile(path.join(__dirname, tempFile), data, (e) => {
+    if (e) throw e;
+  });
+  const python = spawn("python", [parseProductsFileName, tempFile]);
+  return await new Promise((resolve) => {
+    python.stdout.on("data", (res) => {
+      resolve(res.toString());
     });
-    const python = spawn('python', [parseProductsFileName, tempFile]);
-    return await new Promise((resolve) => {
-        python.stdout.on('data', (res) => {
-            resolve(res.toString());
-        })
-    });
+  });
 }
 
 // создает процесс с питоном, парсит карточку продукта
 async function processProductCard(data) {
-    fs.writeFile(path.join(__dirname, tempFile), data, (e) => {
-        if (e) throw e;
+  fs.writeFile(path.join(__dirname, tempFile), data, (e) => {
+    if (e) throw e;
+  });
+  const python = spawn("python", [parseProductCardFileName, tempFile]);
+  return await new Promise((resolve) => {
+    python.stdout.on("data", (res) => {
+      resolve(res.toString());
     });
-    const python = spawn('python', [parseProductCardFileName, tempFile]);
-    return await new Promise((resolve) => {
-        python.stdout.on('data', (res) => {
-            resolve(res.toString());
-        })
-    });
+  });
 }
