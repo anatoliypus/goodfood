d163b6b6703f379d05be4d80a63cf905209045d6
Anatoliy <anatoliypuss5@gmail.com>
2021-02-20 14:49:05 +0300

    comments

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ const app = express()
 app.set('views', 'public')
 app.set('view engine', 'ejs')
 
+// api method to parse data
 app.get('/api/parse', async (req, res) => {
     res.end('Parsing started')
-    console.log('__________________\n')
-    console.log(chalk.yellow('Parsing started'))
+    console.log(chalk.yellow('\nParsing started'))
     const connection = connect()
     await parseEda(connection)
     console.log(chalk.green('Parsing done\n'))
 })
 
+// api method to get recipes json string
 app.get('/api/get', async (req, res) => {
     const connection = connect()
     const data = await getIngredients(connection)
@@ -30,13 +31,10 @@ app.get('/api/get', async (req, res) => {
     res.end(stringified)
 })
 
+// api method to get current recipes amount on resources
 app.get('/api/check', async (req, res) => {
     const data = await checkEdaRAmount()
     res.send(data)
-})
-
-app.get('/test', (req, res) => {
-    res.send(JSON.stringify(req.query))
 })
 
 app.listen(PORT, () => {
