bef9d19aafe1bad39761da35cd890cc28bcbfab4
Anatoliy <anatoliypuss5@gmail.com>
2021-03-06 00:55:11 +0300

    fixed sending to database method

--- database/sendIngredient.js
+++ database/sendIngredient.js
@@ -1,21 +1,20 @@
 import config from './config.js'
+import chalk from 'chalk'
 
 export default function sendIngredient(connection, ingredient) {
-    try {
-        connection.query(
-            `INSERT INTO ${
-                config.tableName
-            } (title, url, cook_time, ingredients_amount, steps_arr_json, ingredients_arr_json, images_arr_json, categories_arr_json)
+    connection.query(
+        `INSERT INTO ${
+            config.tableName
+        } (title, url, cook_time, ingredients_amount, steps_arr_json, ingredients_arr_json, images_arr_json, categories_arr_json)
          VALUES ('${ingredient.title}', '${ingredient.url}', '${
-                ingredient.time
-            }', '${ingredient.ingredientsAmount}', '${JSON.stringify(
-                ingredient.steps
-            )}', '${JSON.stringify(ingredient.ings)}', '${JSON.stringify(
-                ingredient.imgs
-            )}', '${JSON.stringify(ingredient.ctgrs)}')`,
-            (e) => {
-                if (e) throw e
-            }
-        )
-    } catch (e) {}
+            ingredient.time
+        }', '${ingredient.ingredientsAmount}', '${JSON.stringify(
+            ingredient.steps
+        )}', '${JSON.stringify(ingredient.ings)}', '${JSON.stringify(
+            ingredient.imgs
+        )}', '${JSON.stringify(ingredient.ctgrs)}')`,
+        (e) => {
+            if (e) console.log(chalk.red('Error while pushing to db'))
+        }
+    )
 }
--- parsers/gastronom/gastronomParser.js
+++ parsers/gastronom/gastronomParser.js
@@ -8,35 +8,35 @@ const tempFileName = 'parsers/gastronom/temp.txt'
 
 export async function parseGastronom(connection, pagesNumber) {
     return new Promise((resolve) => {
-        const python = spawn('python', [
-            parserFileName,
-            pagesNumber,
-            tempFileName,
-        ])
-        python.on('exit', () => {
-            // console.log('writed file')
-            fs.readFile(tempFileName, (e, data) => {
-                if (e) throw e
-                const json = JSON.parse(data.toString())
-                for (let el of json.list) {
-                    sendIngredient(connection, el)
-                }
-                resolve()
-            })
-        })
+        // const python = spawn('python', [
+        //     parserFileName,
+        //     pagesNumber,
+        //     tempFileName,
+        // ])
+        // python.on('exit', () => {
+        //     // console.log('writed file')
+        //     fs.readFile(tempFileName, (e, data) => {
+        //         if (e) throw e
+        //         const json = JSON.parse(data.toString())
+        //         for (let el of json.list) {
+        //             sendIngredient(connection, el)
+        //         }
+        //         resolve()
+        //     })
+        // })
 
         // python.stdout.on('data', (data) => {
         //     console.log('вывод питона ' + data)
         // })
         // python.stderr.on('', (err) => console.log(err))
 
-        // fs.readFile(tempFileName, (e, data) => {
-        //     if (e) throw e
-        //     const json = JSON.parse(data.toString())
-        //     for (let el of json.list) {
-        //         sendIngredient(connection, el)
-        //     }
-        //     resolve()
-        // })
+        fs.readFile(tempFileName, (e, data) => {
+            if (e) throw e
+            const json = JSON.parse(data.toString())
+            for (let el of json.list) {
+                sendIngredient(connection, el)
+            }
+            resolve()
+        })
     })
 }
