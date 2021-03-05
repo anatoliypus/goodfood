a765b6f08127d45582f9ddbe0ac108b494087dd4
Anatoliy <anatoliypuss5@gmail.com>
2021-03-06 00:34:46 +0300

    modified send method

--- database/sendIngredient.js
+++ database/sendIngredient.js
@@ -1,19 +1,21 @@
 import config from './config.js'
 
 export default function sendIngredient(connection, ingredient) {
-    connection.query(
-        `INSERT INTO ${
-            config.tableName
-        } (title, url, cook_time, ingredients_amount, steps_arr_json, ingredients_arr_json, images_arr_json, categories_arr_json)
+    try {
+        connection.query(
+            `INSERT INTO ${
+                config.tableName
+            } (title, url, cook_time, ingredients_amount, steps_arr_json, ingredients_arr_json, images_arr_json, categories_arr_json)
          VALUES ('${ingredient.title}', '${ingredient.url}', '${
-            ingredient.time
-        }', '${ingredient.ingredientsAmount}', '${JSON.stringify(
-            ingredient.steps
-        )}', '${JSON.stringify(ingredient.ings)}', '${JSON.stringify(
-            ingredient.imgs
-        )}', '${JSON.stringify(ingredient.ctgrs)}')`,
-        (e) => {
-            if (e) throw e
-        }
-    )
+                ingredient.time
+            }', '${ingredient.ingredientsAmount}', '${JSON.stringify(
+                ingredient.steps
+            )}', '${JSON.stringify(ingredient.ings)}', '${JSON.stringify(
+                ingredient.imgs
+            )}', '${JSON.stringify(ingredient.ctgrs)}')`,
+            (e) => {
+                if (e) throw e
+            }
+        )
+    } catch (e) {}
 }
