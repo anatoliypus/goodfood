2d398b9db078ff7ba40b4e2d96499416c9d8d6a7
Anatoliy <anatoliypuss5@gmail.com>
2021-05-11 19:45:59 +0300

    deleted files

--- database/clearIngredients.js
+++
@@ -1,7 +0,0 @@
-import config from './config.js'
-
-export default function clearIngredients(connection) {
-    connection.query(`DELETE FROM ${config.tableName}`, (e) => {
-        if (e) throw new Error()
-    })
-}
